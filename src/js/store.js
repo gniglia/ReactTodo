8b3bb8389650e1aaf8e686bcaf71ae7495ad41d1
Gaston Niglia <gniglia@gmail.com>
2016-08-30 01:50:16 +1200

    Adding actions and reducers Pleast enter the commit messge for your changes. Lines starting g

--- src/js/store.js
+++ src/js/store.js
@@ -1,4 +1,11 @@
-import { applyMiddleware, createStore } from "redux";
-import reducer from "./reducers";
+import { applyMiddleware, createStore } from "redux"
 
-export default createStore(reducer);
+import logger from "redux-logger"
+import thunk from "redux-thunk"
+import promise from "redux-promise-middleware"
+
+import reducer from "./reducers"
+
+const middleware = applyMiddleware(promise(), thunk, logger())
+
+export default createStore(reducer, middleware)
