403e746f3c34baaf9e879e4a48238dd42bf10144
Gaston Niglia <gniglia@gmail.com>
2016-09-04 14:56:40 +1200

    Cleaning fileCleaning fileCleaning fileCleaning files

---
+++ src/js/components/InputPlayer/InputPlayer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const InputPlayer = ({player}) => {
+  return (
+    <div>
+      <label>{player}</label>
+      <input />
+    </div>
+  );
+};
+
+export default InputPlayer;
